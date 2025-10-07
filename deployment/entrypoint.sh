9b5c8a1666078b9cee01268213b9b7530448eab4
Rasul Kireev <me@rasulkireev.com>
2025-10-07 16:55:43 +0300

    remove-otel

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -8,13 +8,7 @@ else
     server=false
 fi
 
-# All commands before the conditional ones
 export PROJECT_NAME=statushen
-
-export OTEL_EXPORTER_OTLP_ENDPOINT=https://signoz-otel-collector-proxy.cr.lvtd.dev
-export OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf
-
-
 export DJANGO_SETTINGS_MODULE="statushen.settings"
 
 while getopts ":sw" option; do
@@ -36,11 +30,7 @@ shift $((OPTIND - 1))
 if [ "$server" = true ]; then
     python manage.py collectstatic --noinput
     python manage.py migrate
-    export OTEL_SERVICE_NAME=${PROJECT_NAME}_${ENVIRONMENT:-dev}
-    export OTEL_RESOURCE_ATTRIBUTES=service.name=${PROJECT_NAME}_${ENVIRONMENT:-dev}
-    opentelemetry-instrument gunicorn ${PROJECT_NAME}.wsgi:application --bind 0.0.0.0:80 --workers 3 --threads 2 --reload
+    gunicorn ${PROJECT_NAME}.wsgi:application --bind 0.0.0.0:80 --workers 3 --threads 2 --reload
 else
-    export OTEL_SERVICE_NAME="${PROJECT_NAME}_${ENVIRONMENT:-dev}_workers"
-    export OTEL_RESOURCE_ATTRIBUTES=service.name=${PROJECT_NAME}_${ENVIRONMENT:-dev}_workers
-    opentelemetry-instrument python manage.py qcluster
+    python manage.py qcluster
 fi
